5db6afaf48fd329c4f4fb71993989d2f2ff05ba5
Hung Doan <hung.doantan@gmail.com>
2020-10-30 10:01:50 +0700

    A workaround fix

--- src/OCHangFire/Startup.cs
+++ src/OCHangFire/Startup.cs
@@ -23,9 +23,9 @@ namespace OCHangFire
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOrchardCms();
-            services.AddHangfire(x => x.UseSqlServerStorage(_configuration.GetConnectionString("Hangfire")));
-            services.AddHangfireServer();
+            services.AddOrchardCms()
+                .ConfigureServices(CmsTenantConfigureServices)
+                .Configure(CmsTenantAppConfigure);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +36,36 @@ namespace OCHangFire
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-            app.UseHangfireDashboard();
+            app.UseRouting(); // NOTE. Also don't use app.UseRouting(); in you main app startup, it is done when we built a tenant pipeline (per tenant)
             app.UseOrchardCore();
+        }
+
+        /// <summary>
+        /// Extend OrchardCore service registrations.
+        /// Reason:
+        /// OrchardCore has a custom logic for the Dependency Injection to isolate registered services in it own tenant instance.
+        /// So that, there are cases/library that can't resolve services from the OrchardCore.
+        /// See problem: https://github.com/OrchardCMS/OrchardCore/issues/7437
+        /// </summary>
+        /// <param name="services"></param>
+        private void CmsTenantConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseSqlServerStorage(_configuration.GetConnectionString("Hangfire")));
+            services.AddHangfireServer();
+        }
+
+        /// <summary>
+        /// Extend OrchardCore application/middleware builder
+        /// Reason:
+        /// OrchardCore has a custom logic for the Dependency Injection to isolate registered services in it own tenant instance.
+        /// So that, there are cases/library that can't resolve services from the OrchardCore.
+        /// See problem: https://github.com/OrchardCMS/OrchardCore/issues/7437
+        /// </summary>
+        /// <param name="app"></param>
+        public void CmsTenantAppConfigure(IApplicationBuilder app)
+        {
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
         }
     }
 }
